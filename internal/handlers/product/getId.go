7b1d16e248f0453a68ebe9b5925768510543391c
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-16 14:57:41 +0100

    Replace static numbers with constants

--- internal/handlers/product/getId.go
+++ internal/handlers/product/getId.go
@@ -2,6 +2,7 @@ package product
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/SnackLog/database-api-wrapper/internal/database/product"
 	"github.com/gin-gonic/gin"
@@ -10,21 +11,21 @@ import (
 func (p *ProductController) GetID(c *gin.Context) {
 	id := c.Param("id")
 	if id == "" {
-		c.JSON(400, gin.H{"error": "ID is required"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID is required"})
 		return
 	}
 
 	productJSON, err := product.GetProductByID(p.DB, id)
 	if err != nil {
 		log.Println(err)
-		c.JSON(500, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
 	if productJSON == "" {
-		c.JSON(404, gin.H{"error": "Product not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
 		return
 	}
 
-	c.JSON(200, gin.H{"product": productJSON})
+	c.JSON(http.StatusOK, gin.H{"product": productJSON})
 }

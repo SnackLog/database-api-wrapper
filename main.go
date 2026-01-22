b6f73897e3cd9ed75951be9deb06dc6a70382503
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-22 16:46:40 +0100

    Remove bogous contact info

--- main.go
+++ main.go
@@ -17,15 +17,6 @@ import (
 // @title           Database API Wrapper
 // @version         1.0
 // @description     API for accessing the SnackLog product database.
-// @termsOfService  http://swagger.io/terms/
-
-// @contact.name   API Support
-// @contact.url    http://www.swagger.io/support
-// @contact.email  support@swagger.io
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
 // @host      localhost:80
 // @BasePath  /api/v1
 

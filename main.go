14d0d6ca30d8cce3c38621a848917d5ea46d3ead
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-22 16:40:39 +0100

    Add swagger comments

--- main.go
+++ main.go
@@ -14,6 +14,25 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
+// @title           Database API Wrapper
+// @version         1.0
+// @description     API for accessing the SnackLog product database.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:80
+// @BasePath  /api/v1
+
+// @securityDefinitions.apikey Bearer
+// @in header
+// @name Authorization
+
 func main() {
 	loadConfigs()
 	db := connectDatabase()

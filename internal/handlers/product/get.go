0ca4b1ae60cbd3b7b381ae193b90fbc803dee470
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-27 15:54:30 +0100

    Update errors to use the new format

--- internal/handlers/product/get.go
+++ internal/handlers/product/get.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/SnackLog/database-api-wrapper/internal/database/product"
+	"github.com/SnackLog/database-api-wrapper/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,15 +19,15 @@ import (
 // @Param        q      query     string  true  "Search query (min 3 chars)"
 // @Param        limit  query     int     false "Limit results (default 10, max 100)"
 // @Success      200  {object}  map[string][]product.Product
-// @Failure      400  {object}  map[string]string
-// @Failure      401  {object}  map[string]string
-// @Failure      500  {object}  map[string]string
+// @Failure      400  {object}  handlers.Error
+// @Failure      401  {object}  handlers.Error
+// @Failure      500  {object}  handlers.Error
 // @Security     Bearer
 // @Router       /products/search [get]
 func (p *ProductController) Get(c *gin.Context) {
 	query := c.Query("q")
 	if query == "" || len(query) < 3 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Query parameter 'q' is required and must be at least 3 characters long"})
+		c.JSON(http.StatusBadRequest, handlers.Error{Error: "Query parameter 'q' is required and must be at least 3 characters long"})
 		return
 	}
 
@@ -40,19 +41,19 @@ func (p *ProductController) Get(c *gin.Context) {
 		if err != nil {
 			log.Println(err)
 		}
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Query parameter 'limit' must be a positive integer less than 100"})
+		c.JSON(http.StatusBadRequest, handlers.Error{Error: "Query parameter 'limit' must be a positive integer less than 100"})
 		return
 	}
 
 	products, err := product.SearchProductByName(p.DB, query, int(limit))
 	if err != nil {
 		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, handlers.Error{Error: err.Error()})
 		return
 	}
 	if products == nil {
 		log.Println("Products was nil?!")
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "products was nil."})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, handlers.Error{Error: "products was nil."})
 		return
 	}
 

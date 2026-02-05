7da4f837fcb060ac6cf398892c1093a075e16697
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-02-05 20:36:15 +0100

    Added quantity to swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -498,6 +498,9 @@ const docTemplate = `{
                 },
                 "product_name": {
                     "type": "string"
+                },
+                "quantity": {
+                    "type": "string"
                 }
             }
         },
